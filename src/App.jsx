bd39e0e4b24278c6db861ce48e7daea9cd5fbc38
Christopher Zoellner <chriszoelln@gmail.come>
2022-10-23 18:15:18 +0200

    fetching account data + can switch networks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,107 @@
-import {useState} from "react"
+import {useState, createContext, useEffect} from "react"
 import {ethers} from "ethers"
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
+export const AppState = createContext()
+
+const { ethereum } = window
+
 function App() {
+
+  const [connected, setconnected] = useState(false)
+  const [currentAccount, setcurrentAccount] = useState("")
+  const [balance, setbalance] = useState("")
+  const [chain, setchain] = useState("")
+  
+  
+  useEffect(() => {
+    cWallet()
+    accountBalance()
+    chainId()
+  }, [])
+
+  ethereum.on("accountsChanged", (accounts) => {
+    window.location.reload()
+  })
+
+  ethereum.on("chainChanged", () => {
+    window.location.reload()
+  })
+
+  // connect wallet
+  const cWallet = async () => {
+    if(ethereum){
+      try {
+        const accounts = await ethereum.request({method: "eth_requestAccounts"})
+        setcurrentAccount(accounts[0])
+        console.log("current account set to ", accounts[0])
+        setconnected(true)
+      } catch (error) {
+        console.log(error)
+      }
+    }else{
+      console.log("ethereum object not found")
+    }
+  }
+
+
+  // get account balance
+  const accountBalance = async () => {
+    const provider = new ethers.providers.Web3Provider(ethereum)
+    const signer =  provider.getSigner()
+    let bal = await signer.getBalance()
+    const convertBal = ethers.utils.formatEther(bal)
+    setbalance(convertBal)
+  }
+
+  const chainId = async () => {
+    let req_chain = await ethereum.request({method: "eth_chainId"})
+    if(req_chain !== "0x5" && req_chain !== "0x1"){
+      alert("Please switch to the mainnet or the goerli testnet to continue!")
+    }
+
+    if(req_chain = "0x5"){
+      setchain("Goerli")
+      console.log("On Goerli Network!")
+    }else if(req_chain = "0x1"){
+      setchain("Mainnet")
+      console.log("On Mainnet!")
+    }else{
+      console.log("chain not recognized !!!!")
+    }
+  }
+
+  const changeToMainnet = async () => {
+    await ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x1' }],
+    })
+
+    setchain("Mainnet")
+  }
+
+  const changeToGoerli = async () => {
+    await ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x5' }],
+    })
+
+    setchain("Goerli")
+  }
+
+
+
   return (
     <div className="w-screen h-screen bg-gradient-to-r from-[#111860] via-indigo-500 to-cyan-600">
-      <Navbar />
-      <Main />
+      <AppState.Provider value={{connected, setconnected, currentAccount, setcurrentAccount, cWallet, accountBalance, balance, setbalance, chainId, chain, setchain, changeToMainnet, changeToGoerli}}>
+        {!connected ? <Navbar /> : (
+          <div>
+          <Navbar/>
+          <Main />
+          </div>
+        )}
+      </AppState.Provider>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppState } from '../App'
 
 const Navbar = () => {
+
+  const App = useContext(AppState)
+
   return (
-    <nav className="flex justify-between container mx-auto items-centet p-6 bg-gradient-to-r from-indigo-500 via-purple-500 to-cyan-500 hover:bg-gradient-to-r transition ease-in-out delay-150s hover:scale-105 duration-500 rounded-3xl text-white">
+    <nav className="flex justify-between container mx-auto items-center p-6 bg-gradient-to-r from-indigo-500 via-purple-500 to-cyan-500 transition  hover:scale-105 duration-500 rounded-3xl text-white">
         <div className="flex justify-center items-center">
           <h3 className="text-3xl font-semibold">Wallet.</h3>
         </div>
 
-        <ul className="flex flex-col justify-between items-center space-y-1">
-          <li className="text-lg font-bold"><a>Network:</a></li>
 
+        {!App.connected ? (
+           <div>
+            <a onClick={App.cWallet} className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-3 px-6 rounded-full text-white cursor-pointer'>Connect</a>
+          </div>
+        ) : (
+          <ul className="flex flex-col justify-between items-center space-y-1">
+          <li className="text-lg font-bold"><a>Networks:</a></li>
+
+            {/* dropdown coming soon */}
           <div className="py-1">
-            <li className="font-semibold"><a>Mainnet</a></li>
-            <li className="font-semibold"><a>Goerli</a></li>
+            <li onClick={App.changeToMainnet} className="font-semibold cursor-pointer"><a>Mainnet</a></li>
+            <li onClick={App.changeToGoerli} className="font-semibold cursor-pointer"><a>Goerli</a></li>
           </div>
         </ul>
+        )}
+
+        
       </nav>
   )
 }
